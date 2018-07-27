38c8c105d20077b445b2bd61818f88f1ddba1e9f
Joshua Dechant <josh@codecrafting.org>
2018-07-27 14:42:36 -0400

    Use __construct and not the class name

--- auth.php
+++ auth.php
@@ -19,7 +19,7 @@ class auth_plugin_enrolmentor extends auth_plugin_base {
     /**
      * Constructor.
      */
-    function auth_plugin_enrolmentor() {
+    public function __construct() {
         $this->authtype = 'enrolmentor';
         $this->config = get_config('auth_enrolmentor');
     }
